6746c5e8bd2a48cfdb844776bfbeaa98d6ef2aa4
Mohaned Mohamed <mohaned.mohamed9999@gmail.com>
2021-06-18 23:51:31 +0200

    Fourth commit

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -4,8 +4,6 @@ class HardWorker
   def perform(id)
     # Do something
     post = @posts.find(id)
-    tags = @tags.where(post_id: id).destroy_all
-    comments = @comments.where(post_id: id).destroy_all
     post.destroy
   end
 end
